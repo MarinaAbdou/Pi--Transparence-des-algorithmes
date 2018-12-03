0ed64e9a4292b3d183dc87432921bd88b9f33f16
AlessandroBusato <45287098+AlessandroBusato@users.noreply.github.com>
2018-12-03 11:16:35 +0100

    Update NN.py

--- NN.py
+++ NN.py
@@ -6,30 +6,24 @@ from keras import backend as K
 from tensorflow import metrics
 
 #path = '/Users/alessandrobusato/Desktop/ESILV/Project/Code/train.csv'
-path = 'clean_train.csv'
-data = pd.read_csv(path, header=0)
-data = data.values
-rows, columns = data.shape
-traindim = 2000
-testdim = 500
-x=data[:,:-1]
-y=data[:,-1]
-x= tf.keras.utils.normalize(x, axis=1)
-x_test = x[-testdim:,:]
-y_test = y[-testdim:]
-x_train = x[:traindim,:]
-y_train = y[:traindim]
+trainpath = 'sample_train.csv'
+testpath = 'sample_test.csv'
+train = pd.read_csv(trainpath, header=0)
+test = pd.read_csv(testpath, header=0)
+train = train.values
+test = test.values
+trrows, trcolumns = train.shape
+terows, tecolumns = test.shape
+x_train=train[:,:-1]
+y_train=train[:,-1]
+x_test=test[:,:-1]
+y_test=test[:,-1]
 
-def roc(y_true, y_pred):
-   score, up_opt = metrics.roc(y_true, y_pred)
-   K.get_session().run(tf.local_variables_initializer())
-   with tf.control_dependencies([up_opt]):
-       score = tf.identity(score)
-   return score
 
 model = keras.Sequential()
-model.add(keras.layers.Dense(141, input_dim=141, kernel_initializer='normal', activation='relu'))
-model.add(keras.layers.Dense(141, input_dim=141, kernel_initializer='normal', activation='relu'))
+model.add(keras.layers.Dense(trcolumns-1, input_dim=trcolumns-1, kernel_initializer='normal', activation='relu'))
+model.add(keras.layers.Dense(trcolumns-1, input_dim=trcolumns-1, kernel_initializer='normal', activation='relu'))
+model.add(keras.layers.Dense(trcolumns-1, input_dim=trcolumns-1, kernel_initializer='normal', activation='relu'))
 model.add(keras.layers.Dense(1, kernel_initializer='normal', activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(x_train,y_train,epochs=3)
