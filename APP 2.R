6bec215173146d5e85335cf3dc778a009e1e49d1
EnricoArlanch <45287575+EnricoArlanch@users.noreply.github.com>
2018-12-15 17:21:31 +0100

    Add files via upload

--- APP 2.R
+++ APP 2.R
@@ -14,14 +14,49 @@ randomSample <- function(x,perc){
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
-
+GoodWrong <- function(Tar,P1,P2){
+  g=0
+  g1=0
+  g2=0
+  ng=0
+  l=length(Tar[,1])
+  for (i in 1:l){
+    if(Tar[i,1]==P1[i,1] &Tar[i,1]==P2[i,1] ){
+      g=g+1
+    }
+    else if(Tar[i,1]==P1[i,1] &Tar[i,1]!=P2[i,1] ){
+      g1=g1+1
+    }
+    else if(Tar[i,1]!=P1[i,1] &Tar[i,1]==P2[i,1] ){
+      g2=g2+1
+    }
+    else {
+      ng=ng+1
+    }
+  }
+  g=g/l
+  g1=g1/l
+  g2=g2/l
+  ng=ng/l
+  
+  dt=data.frame("%number obs"=c(l),
+                "%GOOD PREDICTION BOTH MODELS"=c(g),
+                "%GOOD PREDICTION ONLY 1"=c(g1),
+                "%GOOD PREDICTION ONLY 2"=c(g2),
+                "%BOTH WRONG PRED"=c(ng)
+                )
+  return(dt)
+}
 
 ui <- fluidPage(
-
+  
+  
 # output varariables that represent generic info: n if simulation, train features, time/prec 
   textOutput("N_Sim"),
   tableOutput("InfoSim"),  
-
+  tableOutput("PrecTime"),
+  tableOutput("GW"),
+  
   
 #buttons for next and prev simulation
   actionButton("Next","Next Simulation"),
@@ -52,12 +87,12 @@ ui <- fluidPage(
               max = 33, value = c(0, 33)),
   sliderInput("num_var42", "num_var42: select a range", min = 0, 
               max = 18, value = c(0, 18)),
-  numericInput("ind_var5", "ind_var5: select a value",0,min=0,max=1,step=1)
+  numericInput("ind_var5", "ind_var5: select a value",0,min=0,max=1,step=1),
   
- 
+  tableOutput("GWsubset")
 )
 
-server <- function(input, output, session){
+server <- function(input, output){
   # setting of directory
   path <- "/Users/user/Desktop/Pi2"
   setwd(path)
@@ -66,40 +101,61 @@ server <- function(input, output, session){
   #                  result( fake data)
   RESULTS<- read.csv("results.csv", header=TRUE)
   TEST<- read.csv("sample_test.csv",header=TRUE)
+ 
   # trasport RESULTS and add colum name "M1","M2, ecc,find the  numb. of simulations
   RESULTS<-as.data.frame(t(RESULTS))
   colnames(RESULTS)=c("M1","M2","M1","M2")
   nSim=length(RESULTS)/2
   
+  #create the prediction table
+  PREDICTION=RESULTS
+  PREDICTION[PREDICTION<0.5]=0
+  PREDICTION[PREDICTION>=0.5]=1
+  
+  #create table for good/wrong predictions
+  
+  
   #fake Trainfeat and infoSimof 2 simulation
   trainFeat=data.frame("Level Of Noise"=c(1,0),
                        "% Data Used"=c(50,75),
-                       "%Var Used"=c(100,20),
-                       "prec NN"=c(100,20) , 
-                       "PrecLR"=c(100,20),
-                       "TIme NN"=c(100,20),
-                       "TIME LR"=c(100,20))
+                       "%Var Used"=c(100,20)
+                       )
+  precTime=data.frame(c(10,0.90),c(3,0.88),c(15,0.95),c(5,0.9), row.names = c("time","prec"))
+  colnames(precTime)=c("NN","LR","NN","LR")
   
   
-
+  
   
   v<- reactiveValues(simSelected=data.frame(TEST,RESULTS[1:2]),
                      idSim=1, 
-                     indSubset=c(1:length(TEST[,1])),  #we will use this vector to use only the data of the subset selected
-                     v1 = 0,v2=0, v3=c(0,33),v4=c(0,18) ,v5=0)
-                     infoTable=data.frame("NN"=c(10,0.90),"LR"=c(3,0.88),row.names = c("time","prec"))
+                     indSubset=c(1:length(TEST[,1])),  
+                     v1 = 0,v2=0, v3=c(0,33),v4=c(0,18),v5=0
+                     )
   
   output$N_Sim = renderText(
     print(paste("Sim. N.",v$idSim))
-   
   )
   
   output$InfoSim=renderTable(
     trainFeat[v$idSim,]
  )
   
+  output$PrecTime=renderTable(
+    precTime[(v$idSim*2-1):(v$idSim*2)], rownames = TRUE
+  )
   
+  output$GW=renderTable(
+    GoodWrong(TEST["TARGET"],PREDICTION[v$idSim*2-1],PREDICTION[v$idSim*2])
+  )
   
+  output$GWsubset=renderTable(
+    GoodWrong(data.frame(TEST[v$indSubset,"TARGET"]),
+              data.frame(PREDICTION[v$indSubset,v$idSim*2-1]),
+              data.frame(PREDICTION[v$indSubset,v$idSim*2])
+              )
+  )
+  
+
   
   
   
@@ -160,4 +216,5 @@ server <- function(input, output, session){
 
 
 shinyApp(ui, server)
+
 
