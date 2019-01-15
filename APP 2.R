1105ce39cf731b96dddd895d3a473c31eb5cc891
MarinaAbdou <marina.abdou1996@gmail.com>
2019-01-16 00:16:10 +0100

    Update APP 2.R

--- APP 2.R
+++ APP 2.R
@@ -1,195 +1,127 @@
-library(shiny)
-library(BBmisc)
-library(fBasics)
-library(tidyverse)
-library(caret)
-library(ROCR)
+path <- "C:/Users/Marina/Documents/Semestre 9 (2018-2019)/Pi²/Pi2-Transparence-des-algorithmes-AlessandroBusato-patch-1"
+setwd(path)
 
-#
-randomSample <- function(x,perc){
-  nobs=nrow(x)
-  n=as.integer(perc*nobs)
-  toRet=x[sample(nobs,n),]
-  return(toRet)
-}
-normalize <- function(x) {
-  return ((x - min(x)) / (max(x) - min(x)))
-}
-GoodWrong <- function(Tar,P1,P2){
-  g=0
-  g1=0
-  g2=0
-  ng=0
-  l=length(Tar[,1])
-  for (i in 1:l){
-    if(Tar[i,1]==P1[i,1] &Tar[i,1]==P2[i,1] ){
-      g=g+1
-    }
-    else if(Tar[i,1]==P1[i,1] &Tar[i,1]!=P2[i,1] ){
-      g1=g1+1
-    }
-    else if(Tar[i,1]!=P1[i,1] &Tar[i,1]==P2[i,1] ){
-      g2=g2+1
-    }
-    else {
-      ng=ng+1
-    }
-  }
-  g=g/l
-  g1=g1/l
-  g2=g2/l
-  ng=ng/l
-  
-  dt=data.frame("%number obs"=c(l),
-                "%GOOD PREDICTION BOTH MODELS"=c(g),
-                "%GOOD PREDICTION ONLY 1"=c(g1),
-                "%GOOD PREDICTION ONLY 2"=c(g2),
-                "%BOTH WRONG PRED"=c(ng)
-                )
-  return(dt)
-}
+source("App2_Settings.R")
 
 ui <- fluidPage(
-  
-  #includeCSS("styles.css"),
-  
-  # App title ----
-  titlePanel("Title ?"),
-  
-  hr(),
-  
   fluidRow(
-    column(1,
-           #n if simulation
-           h4(textOutput("N_Sim"))),
+    column(6, align = "left",
+           actionButton("Prev","Previous")),
     
-    #buttons for next and prev simulation
-    column(1,
-           actionButton("Prev","Previous Simulation")),
-    
-    column(1,
-           actionButton("Next","Next Simulation"))
+    column(6, align = "right",
+           actionButton("Next","Next"))
   ),
   
+  # App title ----
+  fluidRow(column(7,offset = 2, titlePanel(textOutput("Title")))), 
   hr(),
   
   # output varariables that represent generic info: train features, time/prec 
-  h4("Generic Info :"),
+  h4("Generic Informations :"),
   
   fluidRow(
-    column(3,
+    column(3, align = "center",
            tableOutput("InfoSim")),
     
-    column(2,
+    column(2, align = "center",
            tableOutput("PrecTime")),
     
-    column(6,
+    column(6, align = "center",
            tableOutput("GW"))
   ),
   
   hr(),
   
-  h4("Select your subset :"),
-  
   fluidRow(
-    column(2,
-           numericInput("ind_var30", "ind_var30: select a value",0,min=0,max=1,step=1)),
+    column(6, align = "center",
+           plotOutput("ROCGlob")),
     
-    column(2,
-           numericInput("num_meses_var5_ult3", "num_meses_var5_ult3: select a value",0,min=0,max=3,step=1)),
-    
-    column(2,
-           sliderInput("num_var30", "num_var30: select a range", min = 0, 
-                       max = 33, value = c(0, 33))),
-    
-    column(2,
-           sliderInput("num_var42", "num_var42: select a range", min = 0, 
-                       max = 18, value = c(0, 18))),
-    
-    column(2,
-           numericInput("ind_var5", "ind_var5: select a value",0,min=0,max=1,step=1)),
-    
-    # button to apply or not to apply the filter
-    column(1,
-           actionButton("nf","No filter")),
-    
-    column(1,
-           actionButton("af","Apply filter"))
+    column(6, align = "center",
+           plotOutput("PieGlob"))
   ),
   
   hr(),
   
   fluidRow(
-    column(5,
-           h4("Select a graph :"),
-           selectInput("N", "", c("Default","histo1", "histo2", "Classification Pie Chart", "ROC Curve")),
-           plotOutput("graph")),
+
+    column(4,
+           h4("Select your subset :"),
+           selectInput("ind_var30", "Select a value for ind_var30 : ",c("No Filter","0","1")),
+           selectInput("num_meses_var5_ult3", "Select a value for num_meses_var5_ult3 : ",c("No Filter","0","1","2","3")),
+           sliderInput("num_var30", "Select a range for num_var30 : ", min = 0, max = 33, value = c(0, 33)),
+           sliderInput("num_var42", "Select a range for num_var42 : ", min = 0, max = 18, value = c(0, 18)),
+           selectInput("ind_var5", "Select a value ind_var5 : ",c("No Filter","0","1")),
+           hr(),
+           fluidRow(
+             column(2,
+                    actionButton("nf","No filter")),
+             column(2,
+                    actionButton("af","Apply filter")))),
     
-    column(6,
+    column(7,
            #text in wich we represent nof obs, n of good prediction for both models, only one of them, no one
            h4(textOutput("SubsetInfo")),
-           
-           tableOutput("GWsubset"))
+           tableOutput("GWsubset"),
+           hr(),
+           h4("Select a graph :"),
+           selectInput("N", "", c("Default","histo1", "histo2", "Classification Pie Chart", "ROC Curve")),
+           plotOutput("graph"))
+  ),
+  
+  fluidRow(
+    h4("Distribution of the results for a variable :"),
+    column(4, align = "center",
+           selectInput("VarForResultsDistr","Select a variable to visualize the distribution of the results",c(colnames(TEST[morCorrIndex])))),
+    column(7, align = "center",
+           tableOutput("ResDisTab"))),
+  
+  hr(),
+  fluidRow(
+    column(11,
+           h4(" Distribution of misclassified samples compared to test distribution"),
+           tableOutput("Misobs"))
+  ),
+ 
+  hr(),
+  fluidRow(
+    column(5,
+           h4(""),
+           plotlyOutput("heatmap", width = "100%", height="500px")),
+    column(5,
+           h4(" "),
+           plotlyOutput("heatmaplr", width = "100%", height="500px")),
+    column(5,
+           h4(""),
+           plotlyOutput("cheatmap", width = "100%", height="500px")),
+    column(5,
+           h4(""),
+           plotlyOutput("cheatmaplr", width = "100%", height="500px")
+    )
   )
+  
 )
 
 server <- function(input, output){
-  # setting of directory
-  path <- "C:/Users/Marina/Documents/Semestre 9 (2018-2019)/Pi²/Pi2-Transparence-des-algorithmes-master"
-  setwd(path)
-  
-  # import the data: sample_test(the same that we use for app1)
-  #                  result( fake data)
-  RESULTS<- read.csv("results.csv", header=TRUE)
-  TEST<- read.csv("sample_test.csv",header=TRUE)
- 
-  # trasport RESULTS and add colum name "M1","M2, ecc,find the  numb. of simulations
-  RESULTS<-as.data.frame(t(RESULTS))
-  colnames(RESULTS)=c("M1","M2","M1","M2")
-  nSim=length(RESULTS)/2
-  
-  #create the prediction table
-  PREDICTION=RESULTS
-  PREDICTION[PREDICTION<0.5]=0
-  PREDICTION[PREDICTION>=0.5]=1
-  
-  
-  #create table for good/wrong predictions  
-  GW_glob = data.frame()
-  for(i in 1:nSim){
-    if(i==1){
-      GW_glob <- rbind(GW_glob,GoodWrong(TEST["TARGET"],PREDICTION[2*i-1],PREDICTION[2*i]))
-    }else{
-      GW_glob <- cbind(GW_glob,GoodWrong(TEST["TARGET"],PREDICTION[2*i-1],PREDICTION[2*i]))
-    }
-    
-  }
-  
-  #fake Trainfeat and infoSimof 2 simulation
-  trainFeat=data.frame("Level Of Noise"=c(1,0),
-                       "% Data Used"=c(50,75),
-                       "%Var Used"=c(100,20)
-                       )
-  precTime=data.frame(c(10,0.90),c(3,0.88),c(15,0.95),c(5,0.9), row.names = c("time","prec"))
-  colnames(precTime)=c("NN","LR","NN","LR")
-  
   
   graphName <- reactive(as.character(input$N))
-  
+  varForDisTab <- reactive(as.character(input$VarForResultsDistr))
   v<- reactiveValues(simSelected=data.frame(TEST,RESULTS[1:2]),
                      idSim=1, 
                      indSubset=c(1:length(TEST[,1])),  
                      v1 = 0,v2=0, v3=c(0,33),v4=c(0,18),v5=0
-                     )
-
+  )
   
-  output$N_Sim = renderText(
-    print(paste("Simulation ",v$idSim))
+  #output$N_Sim = renderText(
+  #  print(paste("Simulation ",v$idSim))
+  #)
+  
+  output$Title = renderText(
+    print(paste("Comparison between the results for simulation ",v$idSim))
   )
   
   output$InfoSim=renderTable(
     trainFeat[v$idSim,]
- )
+  )
   
   output$PrecTime=renderTable(
     precTime[(v$idSim*2-1):(v$idSim*2)], rownames = TRUE
@@ -203,14 +135,14 @@ server <- function(input, output){
     GoodWrong(data.frame(TEST[v$indSubset,"TARGET"]),
               data.frame(PREDICTION[v$indSubset,v$idSim*2-1]),
               data.frame(PREDICTION[v$indSubset,v$idSim*2])
-              )
+    )
   )
   
-
   
   
   
-
+  
+  
   observeEvent(input$Next, {
     a=v$idSim
     if(a<nSim){
@@ -236,23 +168,43 @@ server <- function(input, output){
     v$var3=input$num_var30
     v$var4=input$num_var42
     v$var5=input$ind_var5
+    v$indSubset=indexSubset(TEST,v$var1,v$var2,v$var3,v$var4,v$var5)
     
-    #filter variable
-    v$indSubset=which(TEST["ind_var30"]==v$var1 
-                      & TEST["num_meses_var5_ult3"]==v$var2
-                      & TEST["num_var30"]>v$var3[1]
-                      & TEST["num_var30"]<v$var3[2]
-                      & TEST["num_var42"]>v$var4[1]
-                      & TEST["num_var42"]<v$var4[2]
-                      & TEST["ind_var5"]==v$var5)
-
-    })
+  })
   
   
   output$SubsetInfo=renderText(
-    print(paste("Subset Info : Number of observations = ",length(v$indSubset)))
+    print(paste("Subset Informations : Number of observations = ",length(v$indSubset)))
   )
   
+  
+  output$ROCGlob <- renderPlot({
+    predLR <- prediction(RESULTS[2*v$idSim],TEST["TARGET"])
+    predNN <- prediction(RESULTS[2*v$idSim-1],TEST["TARGET"])
+    perfLR <- performance(predLR,'tpr','fpr')
+    perfNN <- performance(predNN,'tpr','fpr')
+    ROCgraph <- plot(perfNN,main="Generic ROC curve",col="red")
+    par(new=TRUE)
+    plot(perfLR,col="blue")
+    abline(0,1)
+    legend("topleft", legend=c("Neural Networks", "Logistic Regression"),
+           col=c("red", "blue"), lty=1:1, cex=0.8)
+  })
+  
+  output$PieGlob <- renderPlot({
+    pie(as.numeric(c(GW_glob[v$idSim*5-3],
+                     GW_glob[v$idSim*5-2], 
+                     GW_glob[v$idSim*5],
+                     GW_glob[v$idSim*5-1])), 
+      
+      labels =c(paste("Classified correctly by BOTH \n Neural Networks AND Logistic Regression : \n",round(GW_glob[v$idSim*5-3]*100,0),"%"),
+                paste("Classified correctly by \n Neural Networks ONLY : \n",round(GW_glob[v$idSim*5-2]*100,0),"%"),
+                paste("Wrongly classified by BOTH \n Neural Networks AND Logistic Regression : \n",round(GW_glob[v$idSim*5]*100,0),"%"),
+                paste("Classified correctly by \n Logistic Regression ONLY : \n", round(GW_glob[v$idSim*5-1]*100,0),"%")),
+      
+      main = "Generic Classification Pie Chart",
+      col = c("#A9EAFE","#CECECE","#FDF1B8","#CECECE"))
+  })
   
   output$graph <- renderPlot({
     
@@ -263,29 +215,31 @@ server <- function(input, output){
       hist(v$simSelected[v$indSubset,"M2"])
       
     }else if(graphName()=="Classification Pie Chart"){
-
-      pie(as.numeric(c(GW_glob[v$idSim*5-3],
-                       GW_glob[v$idSim*5-2], 
-                       GW_glob[v$idSim*5],
-                       GW_glob[v$idSim*5-1])), 
-          labels =c(paste("Classified correctly by BOTH \n Neural Networks AND Logistic Regression : \n",round(GW_glob[v$idSim*5-3]*100,0),"%"),
-                    paste("Classified correctly by \n Neural Networks ONLY : \n",round(GW_glob[v$idSim*5-2]*100,0),"%"),
-                    paste("Wrongly classified by BOTH \n Neural Networks AND Logistic Regression : \n",round(GW_glob[v$idSim*5]*100,0),"%"),
-                    paste("Classified correctly by \n Logistic Regression ONLY : \n", round(GW_glob[v$idSim*5-1]*100,0),"%")),
-          main = "Classification Pie Chart",
+      
+      GWSub <-  GoodWrong(data.frame(TEST[v$indSubset,"TARGET"]),
+                          data.frame(PREDICTION[v$indSubset,v$idSim*2-1]),
+                          data.frame(PREDICTION[v$indSubset,v$idSim*2]))
+      
+      pie(
+        as.numeric(c(GWSub[2],
+                     GWSub[3], 
+                     GWSub[5],
+                     GWSub[4])), 
+        
+        labels =c(paste("Classified correctly by BOTH \n Neural Networks AND Logistic Regression : \n",round(GWSub[2]*100,0),"%"),
+                  paste("Classified correctly by \n Neural Networks ONLY : \n",round(GWSub[3]*100,0),"%"),
+                  paste("Wrongly classified by BOTH \n Neural Networks AND Logistic Regression : \n",round(GWSub[5]*100,0),"%"),
+                  paste("Classified correctly by \n Logistic Regression ONLY : \n", round(GWSub[4]*100,0),"%")),
+        
+          main = "Subset Classification Pie Chart",
           col = c("#A9EAFE","#CECECE","#FDF1B8","#CECECE"))
       
     }else if(graphName()=="ROC Curve"){
-          #Predictions 0/1
-      #predLR <- prediction(PREDICTION[nSim*v$idSim],TEST["TARGET"])
-      #predNN <- prediction(PREDICTION[nSim*v$idSim-1],TEST["TARGET"])
-          #Predictions in %
-      predLR <- prediction(RESULTS[nSim*v$idSim],TEST["TARGET"])
-      predNN <- prediction(RESULTS[nSim*v$idSim-1],TEST["TARGET"])
-      
+      predLR <- prediction(RESULTS[v$indSubset,2*v$idSim],TEST[v$indSubset,"TARGET"])
+      predNN <- prediction(RESULTS[v$indSubset,2*v$idSim-1],TEST[v$indSubset,"TARGET"])
       perfLR <- performance(predLR,'tpr','fpr')
       perfNN <- performance(predNN,'tpr','fpr')
-      ROCgraph <- plot(perfNN,main="ROC curve",col="red")
+      ROCgraph <- plot(perfNN,main="Subset ROC curve",col="red")
       par(new=TRUE)
       plot(perfLR,col="blue")
       abline(0,1)
@@ -293,10 +247,27 @@ server <- function(input, output){
              col=c("red", "blue"), lty=1:1, cex=0.8)
     }
   })
-  
+  output$ResDisTab=renderTable(
+    clusterResults(varForDisTab(),TEST,PREDICTION[,v$idSim*2-1],PREDICTION[,v$idSim*2]),rownames = TRUE
+  )
+  output$Misobs=renderTable(
+    DistVarPred(varForDisTab(),Test[Misclassified(TEST[,length(TEST[1,])],PREDICTION[,v$idSim*2-1])])
+  )
+  output$heatmap <- renderPlotly({
+    simicmat <- cmat[((v$idSim-1)*(grid+1)+1):((v$idSim)*(grid+1)),]
+    plot_ly(z = simicmat, type = "heatmap",x = xaxis, y = yaxis , height = 400, width = 400)})
+  output$heatmaplr <- renderPlotly({
+    simigmat <- gmat[((v$idSim-1)*(grid+1)+1):((v$idSim)*(grid+1)),]
+    plot_ly(z = simigmat, type = "heatmap",x = xaxis, y = yaxis, height = 400, width = 400)})
+  output$cheatmap <- renderPlotly({
+    st<-((v$idSim-1)*((length(cyaxis))+1))
+    fi<-((v$idSim)*(length(cyaxis)))
+    simiccmat <- ccmat[st:fi,]
+    plot_ly(z = simiccmat, type = "heatmap",x = cxaxis, y = cyaxis , height = 400, width = 400)})
+  output$cheatmaplr <- renderPlotly({
+    st<-((v$idSim-1)*((length(cyaxis))+1))
+    fi<-((v$idSim)*(length(cyaxis)))
+    simicgmat <- cgmat[st:fi,]
+    plot_ly(z = simicgmat, type = "heatmap",x = cxaxis, y = cyaxis, height = 400, width = 400)})
 }
-
-
 shinyApp(ui, server)
-
-
